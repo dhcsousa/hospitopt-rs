0c92e493ed2e9baafc45547fcd0a6950a04041c4
Daniel Sousa <81489741+dhcsousa@users.noreply.github.com>
2025-10-12 10:48:14 +0200

    Feature/data simulation (#11)
    * feat: added hospital simulation data
    * feat: added patient simulation data

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20251005_195352_hospitals;
 mod m20251006_210502_patients;
 mod m20251006_211530_beds_per_speciality;
+mod m20251007_182626_rename_speciality_to_triage;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20251005_195352_hospitals::Migration),
             Box::new(m20251006_210502_patients::Migration),
             Box::new(m20251006_211530_beds_per_speciality::Migration),
+            Box::new(m20251007_182626_rename_speciality_to_triage::Migration),
         ]
     }
 }
